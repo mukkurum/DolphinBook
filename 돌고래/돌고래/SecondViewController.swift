3bd0a0d8c7e393e80d17b4573e6db4de06d1d1b3
김태우 <arangi@gimtaeuui-MacBook-Air.local>
2018-06-07 17:46:06 +0900

    태우 180607

---
+++ 돌고래/돌고래/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  돌고래
+//
+//  Created by Kim JungYeon on 2018. 5. 25..
+//  Copyright © 2018년 김 정연. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
